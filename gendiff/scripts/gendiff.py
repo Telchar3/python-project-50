b9c15e172a6f864544025a243e65fdf6f0ee2894
Bulat Dugarov <erdembulat03@gmail.com>
2024-07-11 14:47:05 -0400

    upgrade script for format yaml and yml

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.10
 import argparse
 import json
 import yaml
@@ -23,21 +23,24 @@ def main():
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
     parser.add_argument('-f', '--format', type=str, help='set format of output')
-
     args = parser.parse_args()
     print(f'Comparing files: {args.first_file} and {args.second_file}')
     if args.format:
         print(f'Output format: {args.format}')
-
     if args.format == 'json':
         file1_data = read_json(args.first_file)
         file2_data = read_json(args.second_file)
-        diff = generate_diff(file1_data, file2_data)
-        print(diff)
-
     elif args.format == 'yaml' or args.format == 'yml':
-        file1_dat = read_yaml(args.first_file)
-        file2_dat = read_yaml(args.second_file)
+        file1_data = read_yaml(args.first_file)
+        file2_data = read_yaml(args.second_file)
+    if args.first_file[-4:] == 'json':
+        file1_data = read_json(args.first_file)
+        file2_data = read_json(args.second_file)
+    elif args.first_file[-4:] == 'yaml' or args.first_file[-3:] == 'yml':
+        file1_data = read_yaml(args.first_file)
+        file2_data = read_yaml(args.second_file)
+    diff = generate_diff(file1_data, file2_data)
+    print(diff)
 
 
 if __name__ == '__main__':
